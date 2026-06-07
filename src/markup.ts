10cb2649551343404590d9179ab2ff4b615c757d
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-07 18:57:09 -0300

    Add markup model

---
+++ src/markup.ts
@@ -0,0 +1,5 @@
+export const sectionsMarkup = [
+  '<section><h2>Encontre arte para</h2><p>Curadoria por contexto.</p></section>',
+  '<section><h2>Obras em destaque</h2><p>Selecao inicial.</p></section>',
+  '<section><h2>Artistas em foco</h2><p>Trajetorias acompanhadas.</p></section>'
+];
