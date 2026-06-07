29ac554e35cd948f63a952c18737b54005227f1c
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-07 20:03:47 -0300

    Add route constants

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+export const routes = {
+  home: '/',
+  works: '/obras',
+  artists: '/artistas',
+  findArt: '/encontrar-arte',
+  selection: '/minha-selecao',
+  artistsIntake: '/para-artistas',
+  projects: '/empresas-e-arquitetos',
+  authenticity: '/autenticidade',
+  about: '/sobre',
+  contact: '/contato'
+};
