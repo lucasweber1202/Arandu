b0b693dab3c30df7a896b2d3ca882e83c798a852
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-07 19:00:57 -0300

    Add theme constants

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    red: '#9e3d2c',
+    terracotta: '#b85a3c',
+    wine: '#5a211d',
+    paper: '#f7f0e6',
+    sand: '#e6d2b8',
+    coal: '#201a17',
+    ink: '#2b211d'
+  }
+};
