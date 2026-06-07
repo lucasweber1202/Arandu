f3e17343cb819bfd68a0c375f4828b0c29dfa5e8
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-07 20:05:30 -0300

    Add experience card content

---
+++ content/experienceCards.ts
@@ -0,0 +1,40 @@
+export const environmentCards = [
+  {
+    id: 'casa',
+    title: 'Casa',
+    text: 'Obras para conviver todos os dias, com presenca, memoria e afeto.'
+  },
+  {
+    id: 'apartamento',
+    title: 'Apartamento',
+    text: 'Pecas para espacos compactos, paredes estrategicas e primeiras colecoes.'
+  },
+  {
+    id: 'escritorio',
+    title: 'Escritorio',
+    text: 'Arte para ambientes de trabalho com identidade, sobriedade e presenca autoral.'
+  },
+  {
+    id: 'empresa',
+    title: 'Empresa',
+    text: 'Curadoria para recepcoes, salas de reuniao e espacos institucionais.'
+  }
+];
+
+export const intentCards = [
+  {
+    id: 'primeira-obra',
+    title: 'Primeira obra',
+    text: 'Comece a comprar arte com acompanhamento, criterio e seguranca.'
+  },
+  {
+    id: 'presente-autoral',
+    title: 'Presente autoral',
+    text: 'Uma obra com historia, permanencia e documentacao.'
+  },
+  {
+    id: 'projeto',
+    title: 'Projeto profissional',
+    text: 'Selecao para arquitetura, empresas e ambientes de atendimento.'
+  }
+];
