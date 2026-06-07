d7f1390a19eaa17c4820efd7ea4b376c767f4e82
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-07 20:03:55 -0300

    Add menu model

---
+++ src/menu.ts
@@ -0,0 +1,7 @@
+export const menu = [
+  { label: 'Encontrar arte', href: '/encontrar-arte' },
+  { label: 'Obras', href: '/obras' },
+  { label: 'Artistas', href: '/artistas' },
+  { label: 'Projetos', href: '/empresas-e-arquitetos' },
+  { label: 'Para artistas', href: '/para-artistas' }
+];
