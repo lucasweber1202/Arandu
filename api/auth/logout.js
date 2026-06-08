cb8da98b75e83eb56699e060da29ac40878d4e27
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-08 15:41:24 -0300

    Prune auth route after API consolidation

--- api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-import { clearSessionCookie } from '../_supabase.js';
-
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.setHeader('Set-Cookie', clearSessionCookie());
-  res.end(JSON.stringify({ ok: true, message: 'Sessão encerrada.' }));
-}
