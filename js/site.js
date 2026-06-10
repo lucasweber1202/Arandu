fe960081e5925ac1d1739750c74eb658a97f8f58
lucasweber1202 <lucasweber1202@gmail.com>
2026-06-10 13:41:25 -0300

    Carrega obras vistas recentemente

--- js/site.js
+++ js/site.js
@@ -75,6 +75,7 @@ function loadCentralLoader() {
 function injectPageIntegrations() {
   if (isInternalPage()) return;
   injectScriptOnce('js/arandu-functions.js?v=20260610-ux-1', 'arandu-functions-js');
+  injectScriptOnce('js/arandu-recent.js?v=20260610-ux-1', 'arandu-recent-js');
   if (currentPage() === 'proposta-curatorial.html') injectScriptOnce('js/proposal-api.js?v=20260610-operational-1', 'arandu-proposal-api-js');
 }
 
